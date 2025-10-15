270ea413c9f75731cc9e2153194c04bc36815a44
Dean Zammit <dean.zammiy.23@um.edu.mt>
2025-10-15 17:29:23 +0200

    Removed statistics since they would cause data leakage

--- src/prepare_features.py
+++ src/prepare_features.py
@@ -10,22 +10,7 @@ def prepare_features():
     df["ResultEncoded"] = df["FullTimeResult"].map({"H": 1, "D": 0, "A": -1}) # Encode target
 
     # Derived features
-    df["GoalDifference"] = df["FullTimeHomeGoals"] - df["FullTimeAwayGoals"]
-    df["ShotDifference"] = df["HomeTeamShots"] - df["AwayTeamShots"]
-    df["OnTargetDifference"] = df["HomeTeamShotsonTarget"] - df["AwayTeamShotsonTarget"]
-    df["CornerDifference"] = df["HomeTeamCorners"] - df["AwayTeamCorners"]
-    df["FoulDifference"] = df["HomeTeamFoulsComitted"] - df["AwayTeamFoulsComitted"]
-    df["YellowCardDifference"] = df["HomeTeamYellowCards"] - df["AwayTeamYellowCards"]
     df["OddsDifference_Bet365"] = df["Bet365AwayWinOdds"] - df["Bet365HomeWinOdds"]
-
-    # Match statistics features
-    stats_cols = [
-        "Season", "Date", "HomeTeam", "AwayTeam", "ResultEncoded",
-        "GoalDifference", "ShotDifference", "OnTargetDifference",
-        "CornerDifference", "FoulDifference", "YellowCardDifference"
-    ]
-    df_stats = df[stats_cols].dropna()
-    save_csv(df_stats, "data/features/eng1_data_stats.csv")
 
     # Bookmaker odds features
     odds_cols = [
@@ -39,8 +24,6 @@ def prepare_features():
     # Combined dataset
     modelling_cols = [
         "Season", "Date", "HomeTeam", "AwayTeam", "ResultEncoded",
-        "GoalDifference", "ShotDifference", "OnTargetDifference",
-        "CornerDifference", "FoulDifference", "YellowCardDifference",
         "Bet365HomeWinOdds", "Bet365DrawOdds", "Bet365AwayWinOdds",
         "OddsDifference_Bet365"
     ]
