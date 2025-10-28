1a5030b2d274e362bc06fb86dfc8b4e974ba15c6
Dean Zammit <dean.zammiy.23@um.edu.mt>
2025-10-28 15:12:11 +0100

    Added random forest model, set shuffle to false and applied random seed to each parameter for consistency

--- src/models/xgboost.py
+++ src/models/xgboost.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+import random
 from xgboost import XGBClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import make_scorer, accuracy_score
@@ -7,6 +9,10 @@ from sklearn_genetic.space import Categorical, Continuous, Integer
 from src.models.evaluate_models import evaluate_model, save_results
 
 def run_xgboost(data_path = "data/features/eng1_data_combined.csv"):
+    random_seed = 0 # Seed value is set to 0
+    np.random.seed(random_seed) # Random seed for numpy random number generator is set
+    random.seed(random_seed) # Random seed for python random number generator is set
+
     df = pd.read_csv(data_path) # Loads dataset
 
     train_df = df[df["Season"] < 2023] # Splits training data using 2019 - 2022 Seasons
@@ -36,21 +42,21 @@ def run_xgboost(data_path = "data/features/eng1_data_combined.csv"):
         tree_method = "hist", # Uses a histogram based algorithm for building the trees
     )    
 
-    param_grid = {
-        "learning_rate": Continuous(0.01, 0.3), # Step size at each boosting step
-        "n_estimators": Integer(300, 500), # Number of trees to build
-        "max_depth": Integer(4, 8), # Maximum depth of each tree
-        "min_child_weight": Integer(1, 5), # Minimum sum of instance weights in a child node
-        "gamma": Continuous(0, 2), # Minimum loss reduction required to further split a leaf node
-        "subsample": Continuous(0.6, 1.0), # Fraction of training samples used for each boosting round
-        "colsample_bytree": Continuous(0.6, 1.0), # Fraction of features used by tree
-        "reg_lambda": Continuous(0.1, 3.0), # L2 regularisation term
-        "reg_alpha": Continuous(0.0, 1.0), # L1 regularisation term
-        "scale_pos_weight": Continuous(1.0, 1.5), # Adjusts for class imbalance where 1 = no correction and 1.5 = slightly favors minority classes
-        "grow_policy": Categorical(["depthwise"]) # Tree growth strategy
+    param_grid = { # Random seed applied to each parameter to ensure consistency
+        "learning_rate": Continuous(0.01, 0.3, random_state=random_seed), # Step size at each boosting step
+        "n_estimators": Integer(300, 500, random_state=random_seed), # Number of trees to build
+        "max_depth": Integer(4, 8, random_state=random_seed), # Maximum depth of each tree
+        "min_child_weight": Integer(1, 5, random_state=random_seed), # Minimum sum of instance weights in a child node
+        "gamma": Continuous(0, 2, random_state=random_seed), # Minimum loss reduction required to further split a leaf node
+        "subsample": Continuous(0.6, 1.0, random_state=random_seed), # Fraction of training samples used for each boosting round
+        "colsample_bytree": Continuous(0.6, 1.0, random_state=random_seed), # Fraction of features used by tree
+        "reg_lambda": Continuous(0.1, 3.0, random_state=random_seed), # L2 regularisation term
+        "reg_alpha": Continuous(0.0, 1.0, random_state=random_seed), # L1 regularisation term
+        "scale_pos_weight": Continuous(1.0, 1.5, random_state=random_seed), # Adjusts for class imbalance where 1 = no correction and 1.5 = slightly favors minority classes
+        "grow_policy": Categorical(["depthwise"], random_state=random_seed) # Tree growth strategy
     }
 
-    cv = StratifiedKFold(n_splits=3, shuffle=True) # Cross validation is set to 3 fold with each fold maintaning the same ratio of outcomes as the full dataset.
+    cv = StratifiedKFold(n_splits=3, shuffle=False) # Cross validation is set to 3 fold with each fold maintaning the same ratio of outcomes as the full dataset.
 
     ga_search = GASearchCV( # Grid search is setup to find the best combination of parameters through cross validation
         estimator = base_model, # The model which is being optmised
@@ -68,10 +74,10 @@ def run_xgboost(data_path = "data/features/eng1_data_combined.csv"):
         criteria = "max" # Scoring function will be maximised
     )
 
-    print("Running genetic algorithm.") # Prints message to reassure that the genetic algorithm is running
+    print("Running Genetic Algorithm.") # Prints message to reassure that the genetic algorithm is running
     ga_search.fit(X_train, y_train) # Parameter tuning is performed
 
-    print("Genetic algorithm Complete. Best Parameters:") # Prints confirmation message that genetic algorithm completed successfully
+    print("Genetic Algorithm Complete. Best Parameters:") # Prints confirmation message that genetic algorithm completed successfully
     print(ga_search.best_params_) # Best parameters found from the genetic algorithm are printed
     print(f"Best Cross Validation Accuracy: {ga_search.best_score_:.4f}") # Best cross validation accuracy is printed
 
